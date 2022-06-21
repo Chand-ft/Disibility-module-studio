c13c470a250ce15509281e2763a664dfbd05eae8
Chand-ft <cftjingaete@gmail.com>
2022-06-22 00:39:22 +0200

    arrays added

--- src/app/components/base_layout.component.ts
+++ src/app/components/base_layout.component.ts
@@ -52,6 +52,7 @@ export class base_layoutComponent {
 
   sd_iCQjSUXKDPzGNIA0(bh) {
     try {
+      bh.local = { displayedObject: [] };
       bh = this.sd_lDUH7s1A5X2UPApW(bh);
       //appendnew_next_sd_iCQjSUXKDPzGNIA0
       return bh;
@@ -60,14 +61,110 @@ export class base_layoutComponent {
     }
   }
 
+  displayFourIcons(...others) {
+    try {
+      var bh: any = this.__page_injector__
+        .get(SDPageCommonService)
+        .constructFlowObject(this);
+      bh.input = {};
+      bh.local = {};
+      bh = this.sd_TtBr0oVhM2W200jK(bh);
+      //appendnew_next_displayFourIcons
+    } catch (e) {
+      return this.errorHandler(bh, e, 'sd_MXTPT7GxtzmdSv2r');
+    }
+  }
+
+  displayDatePicker(...others) {
+    try {
+      var bh: any = this.__page_injector__
+        .get(SDPageCommonService)
+        .constructFlowObject(this);
+      bh.input = {};
+      bh.local = {};
+      bh = this.sd_SHAMiSLwBF0hl8uH(bh);
+      //appendnew_next_displayDatePicker
+    } catch (e) {
+      return this.errorHandler(bh, e, 'sd_jGylx1eY26dsGy2n');
+    }
+  }
+
+  displayEligibility(...others) {
+    try {
+      var bh: any = this.__page_injector__
+        .get(SDPageCommonService)
+        .constructFlowObject(this);
+      bh.input = {};
+      bh.local = {};
+      bh = this.sd_vGVMovbxqKVow60F(bh);
+      //appendnew_next_displayEligibility
+    } catch (e) {
+      return this.errorHandler(bh, e, 'sd_TIZD6HJOivpsG444');
+    }
+  }
+
   //appendnew_flow_base_layoutComponent_start
 
   sd_lDUH7s1A5X2UPApW(bh) {
     try {
+      bh = this.sd_rmLke8YTR9aFJI6M(bh);
       //appendnew_next_sd_lDUH7s1A5X2UPApW
       return bh;
     } catch (e) {
       return this.errorHandler(bh, e, 'sd_lDUH7s1A5X2UPApW');
+    }
+  }
+
+  sd_rmLke8YTR9aFJI6M(bh) {
+    try {
+      const page = this.page;
+      page.displayedObject = [
+        { icon: 'home', paragraph: 'i need to protect my income or family' },
+        { icon: 'group', paragraph: 'i need to protect my family' },
+        {
+          icon: 'account balance wallet',
+          paragraph: 'i need to protect my income',
+        },
+      ];
+      console.log('three icons', page.displayedObject);
+      //appendnew_next_sd_rmLke8YTR9aFJI6M
+      return bh;
+    } catch (e) {
+      return this.errorHandler(bh, e, 'sd_rmLke8YTR9aFJI6M');
+    }
+  }
+
+  sd_TtBr0oVhM2W200jK(bh) {
+    try {
+      const page = this.page;
+      page.displayedObject = [
+        { icon: 'accessible', paragraph: 'Disability Insurance' },
+        { icon: 'airport shuttle', paragraph: 'Accidental Insurance' },
+        { icon: 'group add', paragraph: 'Life Insurance' },
+        { icon: 'grain', paragraph: 'Discount Plans' },
+      ];
+      //appendnew_next_sd_TtBr0oVhM2W200jK
+      return bh;
+    } catch (e) {
+      return this.errorHandler(bh, e, 'sd_TtBr0oVhM2W200jK');
+    }
+  }
+
+  sd_SHAMiSLwBF0hl8uH(bh) {
+    try {
+      //appendnew_next_sd_SHAMiSLwBF0hl8uH
+      return bh;
+    } catch (e) {
+      return this.errorHandler(bh, e, 'sd_SHAMiSLwBF0hl8uH');
+    }
+  }
+
+  sd_vGVMovbxqKVow60F(bh) {
+    try {
+      //appendnew_next_sd_vGVMovbxqKVow60F
+      return bh;
+    } catch (e) {
+      return this.errorHandler(bh, e, 'sd_vGVMovbxqKVow60F');
     }
   }
 
